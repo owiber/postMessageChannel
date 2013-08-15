377deacaafdc91dc8e9c352abe535ab5ee679dc7
Oliver Wong <oliver@owiber.com>
2013-08-15 16:32:03 -0500

    updates, timeout, etc.

--- postmessagechannel.js
+++ postmessagechannel.js
@@ -4,8 +4,10 @@
   var readyMethod = '__postMessageChannel_ready';
   var utils = {};
 
-  // @license MIT - promiscuous library - ©2013 Ruben Verborgh - https://github.com/RubenVerborgh/promiscuous/
-  (function(){function e(){var c,u,f={then:function(e,n){return c(e,n)}};return function(){var i=[];c=function(n,r){var t=e();return i.push({d:t,resolve:n,reject:r}),t.promise},u=function(e,v,s){for(var a=0,p=i.length;p>a;a++){var h=i[a],l=h.d,d=h[e];typeof d!==t?l[e](v):r(d,v,l)}c=n(f,v,s),u=o}}(),{resolve:function(e){u("resolve",e,!0)},reject:function(e){u("reject",e,!1)},promise:f}}function n(n,o,c){return function(u,f){var i,v=c?u:f;return typeof v!==t?n:(setTimeout(r.bind(n,v,o,i=e())),i.promise)}}function r(e,n,r){try{var o=e(n);o&&typeof o.then===t?o.then(r.resolve,r.reject):r.resolve(o)}catch(c){r.reject(c)}}var t="function",o=function(){};utils.promiscuous={resolve:function(e){var r={};return r.then=n(r,e,!0),r},reject:function(e){var r={};return r.then=n(r,e,!1),r},deferred:e}})();
+  (function (window) {
+    // @license MIT - promiscuous library - ©2013 Ruben Verborgh - https://github.com/RubenVerborgh/promiscuous/
+    (function(){function e(){var c,u,f={then:function(e,n){return c(e,n)}};return function(){var i=[];c=function(n,r){var t=e();return i.push({d:t,resolve:n,reject:r}),t.promise},u=function(e,v,s){for(var a=0,p=i.length;p>a;a++){var h=i[a],l=h.d,d=h[e];typeof d!==t?l[e](v):r(d,v,l)}c=n(f,v,s),u=o}}(),{resolve:function(e){u("resolve",e,!0)},reject:function(e){u("reject",e,!1)},promise:f}}function n(n,o,c){return function(u,f){var i,v=c?u:f;return typeof v!==t?n:(setTimeout(r.bind(n,v,o,i=e())),i.promise)}}function r(e,n,r){try{var o=e(n);o&&typeof o.then===t?o.then(r.resolve,r.reject):r.resolve(o)}catch(c){r.reject(c)}}var t="function",o=function(){};window.promiscuous={resolve:function(e){var r={};return r.then=n(r,e,!0),r},reject:function(e){var r={};return r.then=n(r,e,!1),r},deferred:e}})();
+  }(utils));
 
   function Deferred () {
     return utils.promiscuous.deferred();
@@ -84,10 +86,6 @@
       });
     }
 
-    this.ready = function (fn) {
-      readyDfd.promise.then(fn);
-    }
-
     this.addMethod = function (method, fn) {
       methods[method] = fn;
     };
@@ -96,13 +94,16 @@
       delete methods[method];
     };
 
-    this.run = function (method, data) {
+    this.run = function (method, data, timeout) {
       var dfdId = dfds.length;
       var dfd = Deferred();
       dfds.push(dfd);
-      this.ready(function () {
+      readyDfd.promise.then(function () {
         sendMessage(method, data, dfdId);
       });
+      if (timeout) {
+        window.setTimeout(dfd.reject, timeout);
+      }
       return dfd.promise;
     };
     this.reset();
